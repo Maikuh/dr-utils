500e24de719803e134137e94a8576c64e688d19f
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2025-12-29 11:30:03 -0400

    misc script changes

--- scripts/generate-rnc-cedulas.ts
+++ scripts/generate-rnc-cedulas.ts
@@ -14,19 +14,19 @@ const response = await fetch(url)
 await Bun.write(zipPath, response)
 
 console.log('Extracting ZIP...')
-await extractZip(zipPath, { dir: path.join(__dirname, '../../tmp') })
+await extractZip(zipPath, { dir: path.resolve('tmp') })
 
 Bun.file(zipPath).delete()
 
 let txtData = ''
 
+console.log('Reading TXT...')
 const readStream = Bun.file('tmp/TMP/DGII_RNC.TXT').stream()
 
 for await (const chunk of readStream) {
 	txtData += new TextDecoder().decode(chunk)
 }
 
-console.log('Finished reading TXT.')
 console.log('Filtering Cedulas/RNCs...')
 
 const split = txtData.split(/\r?\n/).map((line) => line.split('|')[0]!)
@@ -34,7 +34,7 @@ const split = txtData.split(/\r?\n/).map((line) => line.split('|')[0]!)
 const rncs = durstenfeldShuffle(split.filter(validateRNC)).slice(0, 1000)
 const cedulas = durstenfeldShuffle(split.filter(validateCedula)).slice(0, 1000)
 
-console.log('Writing Cedulas/RNCs to file')
+console.log('Writing Cedulas/RNCs to JSON files...')
 
 await Promise.all([
 	Bun.write('assets/cedulas.json', JSON.stringify(cedulas)),
