9734f05d5b7cb7bdecfc0ef39e9f1587824e91d0
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2025-12-29 03:09:34 -0400

    Change LICENSE to MIT

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,21 +1,3 @@
-/*
-dr-utils: Utilities relevant to the Dominican Republic
-Copyright (C) 2026  Miguel Araujo
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 // Needed to type tests' functions properly without importing them explicitly
 // By default, it thinks the functions are Jest's or Mocha's (e.g. describe, it, etc.)
 /// <reference types="bun-types/test-globals" />
